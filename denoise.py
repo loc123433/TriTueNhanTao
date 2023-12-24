25dff139210e3fffc25e931b17fdb229eb820350
nhutminh2002 <lehuynhnhutminh1722002@gmail.com>
2023-12-24 19:51:12 +0700

    themcode

--- denoise.py
+++ denoise.py
@@ -1,1 +1,20 @@
-################################
+import cv2
+
+def denoise_image(image_path):
+    # Đọc ảnh từ đường dẫn
+    image = cv2.imread(image_path)
+
+    # Áp dụng Median filtering để làm giảm nhiễu
+    denoised_image = cv2.medianBlur(image, 5)  # Kích thước kernel = 5
+
+    # Hiển thị ảnh gốc và ảnh đã được làm giảm nhiễu
+    cv2.imshow("Original Image", image)
+    cv2.imshow("Denoised Image", denoised_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Đường dẫn đến ảnh
+image_path = "NoiseKT(2).jpg"
+
+# Gọi hàm denoise_image với đường dẫn ảnh
+denoise_image(image_path)
