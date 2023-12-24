378f144e9c02761726bf2be5e5ea5337059a4ce7
loc123433 <153848651+loc123433@users.noreply.github.com>
2023-12-24 23:35:52 +0700

    denoise.py

--- denoise.py
+++ denoise.py
@@ -6,3 +6,7 @@ def denoise_image(image_path):
 
     # Áp dụng Median filtering để làm giảm nhiễu
     denoised_image = cv2.medianBlur(image, 5)  # Kích thước kernel = 5
+
+    # Hiển thị ảnh gốc và ảnh đã được làm giảm nhiễu
+    cv2.imshow("Original Image", image)
+    cv2.imshow("Denoised Image", denoised_image)
