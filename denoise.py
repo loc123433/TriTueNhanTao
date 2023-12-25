58767fc3b530fda2e0c4e684f2e40c3e1c3665c3
nttd06 <thuyduong.nguyen2245@gmail.com>
2023-12-25 08:50:23 +0700

    denoise.py
    deniused_image

--- denoise.py
+++ denoise.py
@@ -6,13 +6,13 @@ def denoise_image(image_path):
 
     # Áp dụng Median filtering để làm giảm nhiễu
     denoised_image = cv2.medianBlur(image, 5)  # Kích thước kernel = 5
-
+    #deniused_image
     # Hiển thị ảnh gốc và ảnh đã được làm giảm nhiễu
     cv2.imshow("Original Image", image)
     cv2.imshow("Denoised Image", denoised_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+ 
     # Đường dẫn đến ảnh
 image_path = "NoiseKT(2).jpg"
 
